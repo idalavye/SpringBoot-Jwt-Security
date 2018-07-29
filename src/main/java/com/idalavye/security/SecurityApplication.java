c659a38280171b706cdca98d051b7c64a59cbd7b
DESKTOP-TCRB5HT\idalavye <idalavye@gmail.com>
2018-07-29 14:58:30 +0300

    adding new classes

--- src/main/java/com/idalavye/security/SecurityApplication.java
+++ src/main/java/com/idalavye/security/SecurityApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityApplication {
 
 	public static void main(String[] args) {
+
+		//deneme
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 }
